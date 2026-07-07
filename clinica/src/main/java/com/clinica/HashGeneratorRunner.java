498e2e128f2ae10552966a19a4ebf95d59ed1bbf
Git-Fabiano-Git <fabiano.ti.dev@gmail.com>
2026-07-07 20:28:43 -0300

    Update HashGeneratorRunner.java

--- clinica/src/main/java/com/clinica/HashGeneratorRunner.java
+++ clinica/src/main/java/com/clinica/HashGeneratorRunner.java
@@ -10,12 +10,20 @@ public class HashGeneratorRunner implements CommandLineRunner {
     @Override
     public void run(String... args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String hash = encoder.encode("admin123");
+
+        String hashAdmin = encoder.encode("admin123");
+        String hashMedico = encoder.encode("medico123");
+        String hashPaciente = encoder.encode("paciente123");
+
         System.out.println("============================================");
-        System.out.println("Hash BCrypt para 'admin123':");
-        System.out.println(hash);
+        System.out.println("Hash para 'admin123':    " + hashAdmin);
+        System.out.println("Hash para 'medico123':   " + hashMedico);
+        System.out.println("Hash para 'paciente123': " + hashPaciente);
         System.out.println("============================================");
     }
 }
 
-// hash gerado:     $2a$10$SzycFVI22jD8AHh7MNVW3.xKzs8onkz11U8bFHXLWiu0t4ZA5s9pG
+// 1o hash gerado para admin:     $2a$10$SzycFVI22jD8AHh7MNVW3.xKzs8onkz11U8bFHXLWiu0t4ZA5s9pG
+// 1o hasg para medico:       $2a$10$sFqkCSLorDy4SBlCs9NX5uQQTCIFQJrr9GOKNS0bSP9j45Y6AUn8y
+// 1o hash para paciente:         $2a$10$6bMFNHIl/fLDgo7cKn9AB.PAbUr/y7phshpaPIxaoB00PkaJIlj8a
+// qualquer um deve funcionar
